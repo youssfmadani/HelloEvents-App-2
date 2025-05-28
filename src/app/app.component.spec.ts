e8bbe9b8518531431ad608d15339b73380c42793
youssfmadani <youssfelmadani@gmail.com>
2025-05-28 10:18:49 +0100

    Ajout des composants necessaires avec services admin et client

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Event-management-frontend' title`, () => {
+  it(`should have the 'FrontendHelloEventsApp' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Event-management-frontend');
+    expect(app.title).toEqual('FrontendHelloEventsApp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Event-management-frontend');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, FrontendHelloEventsApp');
   });
 });
